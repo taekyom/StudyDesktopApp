8ea8d60cb19753cccf3e4238a6b38464e5992d39
taekyom <tkyoung1014@naver.com>
2021-03-15 14:27:35 +0900

    210315 로그인 커밋

---
+++ WPFApp/WpfAdvBank/WpfLoginApp/LoginWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLoginApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connString = "Data Source=127.0.0.1;Initial Catalog=PMS;Persist Security Info=True;" +
+                            "User ID=sa;Password=mssql_p@ssw0rd!";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            TxtUserName.Focus();
+        }
+
+        private void TxtUserName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                TxtUserPwd.Focus();
+            }
+        }
+
+        private void TxtUserPwd_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                BtnLogin_Click(sender, e);
+            
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if(conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                try
+                {
+                    string query = $"SELECT count(*) " +
+                               $"  FROM Member " +
+                               $" WHERE UserId = '{TxtUserName.Text}'" +
+                               $"   AND Password = '{TxtUserPwd.Password}'; "; //각 행마다 띄어쓰기 중요! 한 줄에 적을 때를 생각하면서 줄바꿈할 것
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    var result = Convert.ToInt32(cmd.ExecuteScalar()); //parameter가 object  
+
+                    if (result == 1)
+                        MessageBox.Show("로그인 성공");
+                    else
+                        MessageBox.Show("로그인 실패");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"예외발생 : {ex.Message}");
+                    return;
+                }
+                
+            }
+        }
+    }
+}
---
+++ WinformApp/WinFormAdvancedBank/WinChartApp/FrmMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WinChartApp
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRegen_Click(object sender, EventArgs e)
+        {
+            GenNewChart();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            this.Text = "중간고사 성적"; //frmmain의 title
+            ChtScore.Titles.Add("중간고사 성적"); //chart의 title
+            GenNewChart();
+        }
+
+        private void GenNewChart()
+        {
+            Random rand = new Random();
+            ChtScore.Series[0].Points.Clear(); 
+            for (int i = 0; i < 10; i++)
+            {
+                ChtScore.Series[0].Points.Add(rand.Next(10, 100)); //series : data 하나하나를 나타냄, 배열임 ex)series1 : score, series2 : average ...
+            }
+            ChtScore.Series[0].LegendText = "수학";
+            ChtScore.Series[0].ChartType = SeriesChartType.Column;
+
+
+        }
+    }
+}
