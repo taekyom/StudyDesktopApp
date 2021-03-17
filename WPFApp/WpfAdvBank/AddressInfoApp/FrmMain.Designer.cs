bd61c16ee0b58a58cdb785929d6dcad58dcf4b55
taekyom <tkyoung1014@naver.com>
2021-03-17 09:24:06 +0900

    210315 월요일 커밋

---
+++ WPFApp/WpfAdvBank/AddressInfoApp/FrmMain.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace AddressInfoApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvAddress = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnRevise = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.TxtIdx = new System.Windows.Forms.TextBox();
+            this.TxtAddr = new System.Windows.Forms.TextBox();
+            this.TxtFullName = new System.Windows.Forms.TextBox();
+            this.TxtContact = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAddress)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DgvAddress
+            // 
+            this.DgvAddress.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvAddress.Location = new System.Drawing.Point(12, 12);
+            this.DgvAddress.Name = "DgvAddress";
+            this.DgvAddress.RowTemplate.Height = 23;
+            this.DgvAddress.Size = new System.Drawing.Size(699, 293);
+            this.DgvAddress.TabIndex = 0;
+            this.DgvAddress.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvAddress_CellClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TxtContact);
+            this.groupBox1.Controls.Add(this.TxtFullName);
+            this.groupBox1.Controls.Add(this.TxtAddr);
+            this.groupBox1.Controls.Add(this.TxtIdx);
+            this.groupBox1.Controls.Add(this.BtnDelete);
+            this.groupBox1.Controls.Add(this.BtnRevise);
+            this.groupBox1.Controls.Add(this.BtnAdd);
+            this.groupBox1.Controls.Add(this.BtnClear);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 311);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(699, 151);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "입력창";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Idx";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "이름";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "연락처";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "주소";
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(478, 29);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(83, 31);
+            this.BtnClear.TabIndex = 9;
+            this.BtnClear.Text = "초기화";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(478, 81);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(83, 31);
+            this.BtnAdd.TabIndex = 11;
+            this.BtnAdd.Text = "추가";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnRevise
+            // 
+            this.BtnRevise.Location = new System.Drawing.Point(595, 29);
+            this.BtnRevise.Name = "BtnRevise";
+            this.BtnRevise.Size = new System.Drawing.Size(83, 31);
+            this.BtnRevise.TabIndex = 10;
+            this.BtnRevise.Text = "수정";
+            this.BtnRevise.UseVisualStyleBackColor = true;
+            this.BtnRevise.Click += new System.EventHandler(this.BtnRevise_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(595, 81);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(83, 31);
+            this.BtnDelete.TabIndex = 12;
+            this.BtnDelete.Text = "삭제";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // TxtIdx
+            // 
+            this.TxtIdx.Location = new System.Drawing.Point(79, 29);
+            this.TxtIdx.Name = "TxtIdx";
+            this.TxtIdx.ReadOnly = true;
+            this.TxtIdx.Size = new System.Drawing.Size(101, 21);
+            this.TxtIdx.TabIndex = 5;
+            // 
+            // TxtAddr
+            // 
+            this.TxtAddr.Location = new System.Drawing.Point(79, 101);
+            this.TxtAddr.MaxLength = 200;
+            this.TxtAddr.Name = "TxtAddr";
+            this.TxtAddr.Size = new System.Drawing.Size(290, 21);
+            this.TxtAddr.TabIndex = 8;
+            // 
+            // TxtFullName
+            // 
+            this.TxtFullName.Location = new System.Drawing.Point(236, 29);
+            this.TxtFullName.MaxLength = 12;
+            this.TxtFullName.Name = "TxtFullName";
+            this.TxtFullName.Size = new System.Drawing.Size(133, 21);
+            this.TxtFullName.TabIndex = 6;
+            this.TxtFullName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtFullName_KeyPress);
+            // 
+            // TxtContact
+            // 
+            this.TxtContact.Location = new System.Drawing.Point(79, 65);
+            this.TxtContact.Mask = "000-9000-0000";
+            this.TxtContact.Name = "TxtContact";
+            this.TxtContact.Size = new System.Drawing.Size(101, 21);
+            this.TxtContact.TabIndex = 7;
+            this.TxtContact.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtContact_KeyPress);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 474);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.DgvAddress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "주소록 앱";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAddress)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvAddress;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TxtFullName;
+        private System.Windows.Forms.TextBox TxtAddr;
+        private System.Windows.Forms.TextBox TxtIdx;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnRevise;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox TxtContact;
+    }
+}
+
---
+++ WPFApp/WpfAdvBank/AddressInfoApp/FrmMain.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressInfoApp
+{
+    public partial class FrmMain : Form
+    {
+        string connString = "Data Source=127.0.0.1;Initial Catalog=PMS;Persist Security Info=True;" +
+                            "User ID=sa;Password=mssql_p@ssw0rd!";
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            ClearInput();
+        }
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            int.TryParse(TxtIdx.Text, out int result);
+            if(result > 0)
+            {
+                MessageBox.Show("초기화를 하십시오.");
+                return;
+            }
+            if (string.IsNullOrEmpty(TxtFullName.Text) || string.IsNullOrEmpty(TxtContact.Text))
+            {
+                MessageBox.Show("값을 입력하세요.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                string query = $"INSERT INTO dbo.Address " +
+                               $"( FullName, " +
+                               $"  Contact, " +
+                               $"  Addr, " +
+                               $"  RegId, " +
+                               $"  RegDate) " +
+                               $"VALUES " +
+                               $"( '{TxtFullName.Text}', " +
+                               $"  '{TxtContact.Text.Replace("-", "")}', " +
+                               $"  '{TxtAddr.Text}'," +
+                               $"  'admin'," +
+                               $"   GETDATE() ); ";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if (cmd.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("입력성공!");
+                }
+                else
+                {
+                    MessageBox.Show("입력실패!");
+                }
+            }
+            ClearInput();
+            RefreshData();
+        }
+        
+        private void BtnRevise_Click(object sender, EventArgs e)
+        {
+            int.TryParse(TxtIdx.Text, out int result);
+            if (result == 0)
+            {
+                MessageBox.Show("데이터를 선택하십시오.");
+                return;
+            }
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                string query = $"UPDATE Address " +
+                               $" SET " +
+                               $" FullName = '{TxtFullName.Text}', " +
+                               $" Contact = '{TxtContact.Text.Replace("-", "")}'," +
+                               $" Addr = '{TxtAddr.Text}'," +
+                               $" ModId = 'admin'," +
+                               $" ModDate = GETDATE() " +
+                               $" WHERE Idx = {result} ";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if (cmd.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("수정 성공!");
+                }
+                else
+                {
+                    MessageBox.Show("수정 실패!");
+                }
+            }
+            ClearInput();
+            RefreshData();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            //데이터를 선택하지 않고 삭제버튼을 누를 때
+            int.TryParse(TxtIdx.Text, out int result);
+            if(result == 0)
+            {
+                MessageBox.Show("데이터를 선택하십시오.");
+                return;
+            }
+
+            if(MessageBox.Show("삭제하시겠습니까?", "삭제", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                //원하는 데이터 선택 후 삭제버튼 누를 때
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    if (conn.State == ConnectionState.Closed)
+                        conn.Open();
+
+                    string query = $"DELETE FROM Address WHERE idx = {result}";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    if (cmd.ExecuteNonQuery() == 1)
+                    {
+                        MessageBox.Show("삭제 성공!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("삭제 실패!");
+                    }
+                }
+                ClearInput();
+                RefreshData();
+            }
+        }
+
+        private void TxtFullName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13) //엔터
+            {
+                TxtContact.Focus();
+            }
+        }
+
+        private void TxtContact_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                TxtAddr.Focus();
+            }
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            RefreshData();
+            ClearInput();
+        }
+
+        private void RefreshData()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                //SSMS 테이블 스크립팅 메뉴 활용
+                string query = "SELECT Idx " +
+                               "     , FullName " +
+                               "     , Contact " +
+                               "     , Addr " +
+                               "  FROM dbo.Address ";
+                //SqlCommand, SqlDataReader or Object 사용방법1
+                //SqlDataAdapter, DataSet
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+
+                DgvAddress.DataSource = ds.Tables[0];
+            }
+        }
+
+        private void ClearInput()
+        {
+            TxtIdx.Text = TxtFullName.Text = TxtContact.Text = TxtAddr.Text = "";
+        }
+
+        //cell 클릭했을 때 각 textbox에 원래 정보가 바로 입력되도록 하는 부분
+        private void DgvAddress_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var selectData = DgvAddress.Rows[e.RowIndex].Cells;
+
+            TxtIdx.Text = selectData[0].Value.ToString();
+            TxtFullName.Text = selectData[1].Value.ToString();
+            TxtContact.Text = selectData[2].Value.ToString();
+            TxtAddr.Text = selectData[3].Value.ToString();
+        }
+    }
+}
