91a72f8aa61cf3de72c908cb01829f1f5b7f3973
taekyom <tkyoung1014@naver.com>
2021-03-23 16:54:39 +0900

    210323 전체커밋

---
+++ WPFApp/WpfAdvBank/IoTSensorMonApp/FrmMain.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace IoTSensorMonApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 399);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MnuLoadFile;
+        private System.Windows.Forms.ToolStripMenuItem MnuSaveFile;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem MnuExit;
+        private System.Windows.Forms.ToolStripMenuItem 시뮬레이션ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MnuBeginSimulation;
+        private System.Windows.Forms.ToolStripMenuItem MnuEndSimulation;
+        private System.Windows.Forms.Label LblConnectTime;
+        private System.Windows.Forms.TextBox TxtSensorNum;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar PrbPhotoResistor;
+        private System.Windows.Forms.ListBox LsbPhotoResistors;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button BtnDisplay;
+        private System.Windows.Forms.Button BtnDisconnect;
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.ComboBox CboSerialPort;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChtPhotoResistors;
+        private System.Windows.Forms.Button BtnViewAll;
+        private System.Windows.Forms.Button BtnZoom;
+    }
+}
+
