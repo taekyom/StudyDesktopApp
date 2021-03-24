85678a35157ae3cb5b271e83a4413519bca279fe
taekyom <tkyoung1014@naver.com>
2021-03-24 17:22:26 +0900

    210324 오픈 API 커밋

--- WPFApp/WpfAdvBank/IoTSensorMonApp/FrmMain.Designer.cs
+++ WPFApp/WpfAdvBank/IoTSensorMonApp/FrmMain.Designer.cs
@@ -34,6 +34,10 @@ namespace IoTSensorMonApp
             this.시뮬레이션SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.읽어오기OToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.저장하기SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.종료XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.시작BToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.끝EToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,13 +56,18 @@ namespace IoTSensorMonApp
             // 
             this.파일FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.읽어오기OToolStripMenuItem,
-            this.저장하기SToolStripMenuItem});
+            this.저장하기SToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.종료XToolStripMenuItem});
             this.파일FToolStripMenuItem.Name = "파일FToolStripMenuItem";
             this.파일FToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
             this.파일FToolStripMenuItem.Text = "파일(&F)";
             // 
             // 시뮬레이션SToolStripMenuItem
             // 
+            this.시뮬레이션SToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.시작BToolStripMenuItem,
+            this.끝EToolStripMenuItem});
             this.시뮬레이션SToolStripMenuItem.Name = "시뮬레이션SToolStripMenuItem";
             this.시뮬레이션SToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
             this.시뮬레이션SToolStripMenuItem.Text = "시뮬레이션(&S)";
@@ -74,6 +83,29 @@ namespace IoTSensorMonApp
             this.저장하기SToolStripMenuItem.Name = "저장하기SToolStripMenuItem";
             this.저장하기SToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.저장하기SToolStripMenuItem.Text = "저장하기(&S)";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // 종료XToolStripMenuItem
+            // 
+            this.종료XToolStripMenuItem.Name = "종료XToolStripMenuItem";
+            this.종료XToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.종료XToolStripMenuItem.Text = "종료(&X)";
+            // 
+            // 시작BToolStripMenuItem
+            // 
+            this.시작BToolStripMenuItem.Name = "시작BToolStripMenuItem";
+            this.시작BToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.시작BToolStripMenuItem.Text = "시작(&B)";
+            // 
+            // 끝EToolStripMenuItem
+            // 
+            this.끝EToolStripMenuItem.Name = "끝EToolStripMenuItem";
+            this.끝EToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.끝EToolStripMenuItem.Text = "끝(&E)";
             // 
             // FrmMain
             // 
@@ -98,6 +130,10 @@ namespace IoTSensorMonApp
         private System.Windows.Forms.ToolStripMenuItem 읽어오기OToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 저장하기SToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 시뮬레이션SToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 종료XToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 시작BToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 끝EToolStripMenuItem;
     }
 }
 
